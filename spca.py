cf9bd7d51797917f57f491e4a384335ca4226010
Siu Kwan Lam <michael.lam.sk@gmail.com>
2014-08-12 17:45:14 -0500

    Benchmarking radixsort based version

--- spca.py
+++ spca.py
@@ -2,12 +2,14 @@ from __future__ import print_function, division
 import sys
 import numpy as np
 import timeit
+import itertools
 import math
 from numbapro import cuda, float64, int16, int32
-from numbapro.cudalib import curand, cublas
+from numbapro.cudalib import curand, cublas, sorting
 
 try:
     xrange
+    zip = itertools.izip
 except NameError:
     xrange = range
 
@@ -15,7 +17,7 @@ cached_input_file = "input.npy"
 
 
 def generate_input():
-    p = 1000
+    p = 5000
     n = 100
     X = np.random.randn(n, p)
 
@@ -24,6 +26,7 @@ def generate_input():
     A = U.dot(np.diag(S).dot(np.diag(1. / np.arange(1, p + 1)))).dot(
         np.conjugate(U.T))
 
+    print(A.shape)
     return A
 
 
@@ -55,8 +58,6 @@ def spca_unopt(Vd, epsilon=0.1, d=3, k=10):
         if val > opt_v:
             opt_v = val
             opt_x = np.zeros((p, 1))
-            #print((opt_x[I[0:k]]).shape)
-            #print((a[I[0:k]]/val).shape)
             opt_x[I[-k:]] = a[I[-k:], :] / val
 
     return opt_x
@@ -114,108 +115,16 @@ def swapi(ary, a, b):
     ary[b] = t
 
 
-@cuda.jit("void(float64[:,:], int16[:,:], int16)")
-def batch_k_selection(A, I, k):
-    """QuickSelect
-    """
-    sampleIdx = cuda.blockIdx.x
-    tid = cuda.threadIdx.x
-
-    # XXX: hardcoded array size for maximum capability
-    values = cuda.shared.array(shape=1000, dtype=float64)
-    indices = cuda.shared.array(shape=1000, dtype=int16)
-    storeidx = cuda.shared.array(shape=1, dtype=int32)
-    rightidx = cuda.shared.array(shape=1, dtype=int32)
-
-    # Prefill cache
-    values[tid] = A[tid, sampleIdx]
-    indices[tid] = tid
-    cuda.syncthreads()
-
-    st = 0
-    rst = 0
-    n = A.shape[0]
-    left = 0
-    right = n - 1
-    val = 0.0
-    ind = 0
-    while left < right:
-    # for _ in range(1):
-        st = -1
-        rst = -1
-
-        pivot = right  #(right + left + 1) // 2
-
-        storeidx[0] = left
-        rightidx[0] = 0
-
-        pval = values[pivot]
-
-        # Move pivot to the end
-        # if tid == 0:
-        #     print(7777777)
-        #     print(left + 0)
-        #     print(right + 0)
-        #     print(pivot + 0)
-        # swapf(values, right, pivot)
-        # swapi(indices, right, pivot)
-        cuda.syncthreads()
-
-        # Compare
-        if tid >= left and tid < right:
-            val = values[tid]
-            ind = indices[tid]
-            if val < pval:
-                st = cuda.atomic.add(storeidx, 0, 1)
-            else:
-                rst = cuda.atomic.add(rightidx, 0, 1)
-
-        cuda.syncthreads()
-        finalpivot = storeidx[0]
-
-        if rst != -1:
-            # Assign right partition index
-            st = finalpivot + rst
-
-        # Swap
-        if st != -1 and st != tid:
-            values[st] = val
-            indices[st] = ind
-
-        cuda.syncthreads()
-
-        # Move pivot to final destination
-        if tid == 0:
-            swapf(values, finalpivot, right)
-            swapi(indices, finalpivot, right)
-
-        cuda.syncthreads()
-
-        # Adjust range or done
-        remain = n - finalpivot
-        if remain == k:
-            break
-        elif remain > k:
-            left = finalpivot + 1
-        else:
-            right = finalpivot - 1
-
-    if tid < k:
-        I[tid, sampleIdx] = indices[n - tid - 1]
-        # I[tid, sampleIdx] = indices[tid]
-
-
-@cuda.jit("void(float64[:,:], int16[:,:], float64[:])")
-def batch_scatter_norm(A, I, aInorm):
+@cuda.jit("void(float64[:,:], float64[:], int32)")
+def batch_norm(A, aInorm, K):
     tid = cuda.grid(1)
 
-    if tid >= I.shape[1]:
+    if tid >= A.shape[1]:
         return
 
     sum = 0.0
-    for k in range(I.shape[0]):
-        ind = I[k, tid]
-        val = A[ind, tid]
+    for k in range(K):
+        val = A[k, tid]
         sum += val * val
 
     aInorm[tid] = math.sqrt(sum)
@@ -223,28 +132,6 @@ def batch_scatter_norm(A, I, aInorm):
 
 def calc_ncta1d(size, blksz):
     return size + (blksz - 1) // blksz
-
-
-def gpu_slice_view(arr, col):
-
-    strides = arr.strides
-    s = col * strides[1]
-    e = (col + 1) * strides[1]
-    view = arr.gpu_data.view(s, e)
-    # view = DeviceView(arr.gpu_data, s, e)
-    return view, e - s
-
-
-def gpu_slice(arr, col):
-    """
-    Missing feature in NumbaPro
-    """
-    from numba.cuda.cudadrv.driver import device_to_host
-
-    view, size = gpu_slice_view(arr, col)
-    host = np.empty(shape=arr.shape[0], dtype=arr.dtype)
-    device_to_host(host, view, size)
-    return host
 
 
 def spca(Vd, epsilon=0.1, d=3, k=10):
@@ -265,13 +152,19 @@ def spca(Vd, epsilon=0.1, d=3, k=10):
     custr = cuda.stream()
     prng = curand.PRNG(stream=custr)
 
+    rsstr1 = cuda.stream()
+    rsstr2 = cuda.stream()
+    sorter1 = sorting.Radixsort(dtype=np.float64, stream=rsstr1)
+    sorter2 = sorting.Radixsort(dtype=np.float64, stream=rsstr2)
+
     while remaining:
         numSamples = min(remaining, maxSize)
         remaining -= numSamples
 
         # Prepare storage for vector A
         dA = cuda.device_array(shape=(Vd.shape[0], numSamples), order='F')
-        dI = cuda.device_array(shape=(k, numSamples), dtype=np.int16, order='F')
+        dI = cuda.device_array(shape=(k, numSamples), dtype=np.int32,
+                               order='F')
         daInorm = cuda.device_array(shape=numSamples, dtype=np.float64)
         dC = cuda.device_array(shape=(d, numSamples), order='F')
 
@@ -290,11 +183,71 @@ def spca(Vd, epsilon=0.1, d=3, k=10):
 
         # Replaces: I = np.argsort(a, axis=0)
         # Note: the k-selection is dominanting the time
-        batch_k_selection[numSamples, Vd.shape[0], custr](dA, dI, k)
+
+        lastprogress = 0
+        custr.synchronize()
+
+        # Distribute the work to two streams
+
+        async_dA1 = dA.bind(rsstr1)
+        async_dI1 = dI.bind(rsstr1)
+        async_dA2 = dA.bind(rsstr2)
+        async_dI2 = dI.bind(rsstr2)
+        selnext1 = sorter1.batch_argselect(dtype=dA.dtype,
+                                           count=dA.shape[0],
+                                           k=k, reverse=True)
+        selnext2 = sorter2.batch_argselect(dtype=dA.dtype,
+                                           count=dA.shape[0],
+                                           k=k, reverse=True)
+
+        def take2(it):
+            it = iter(it)
+            while True:
+                yield next(it), next(it)
+
+        def work(i, packed):
+            (dA, dI, selnext, stream) = packed
+            # print('read', i)
+            subdA = dA[:, i]
+            yield
+            # print('compute', i)
+            dIi = selnext(subdA)
+            yield
+            # print('write', i)
+            dI[:, i].copy_to_device(dIi, stream=stream)
+            yield
+
+        def exhaust(*args):
+            for _ in zip(*args):
+                pass
+
+        g1 = async_dA1, async_dI1, selnext1, rsstr1
+        g2 = async_dA2, async_dI2, selnext2, rsstr2
+        roundrobin = itertools.cycle([g1, g2])
+
+        seq1 = (exhaust(work(*arg1), work(*arg2))
+                for arg1, arg2
+                in take2(zip(range(numSamples), roundrobin)))
+
+        exhaust(seq1)
+
+        # for i, (async_dA, async_dI, selnext, stream) \
+        #         in zip(range(numSamples), roundrobin()):
+        #     dIi = selnext(async_dA[:, i])
+        #     async_dI[:, i].copy_to_device(dIi, stream=stream)
+        #
+        #     newprogress = i / numSamples
+        #     if newprogress - lastprogress > 0.1:
+        #         print('progress', newprogress)
+        #         lastprogress = newprogress
+
+        rsstr1.synchronize()
+        rsstr2.synchronize()
 
         # Replaces: val = np.linalg.norm(a[I[-k:]])
-        batch_scatter_norm[calc_ncta1d(numSamples, 512), 512, custr](dA, dI,
-                                                                     daInorm)
+        # batch_scatter_norm[calc_ncta1d(numSamples, 512), 512, custr](dA, dI,
+        #                                                              daInorm)
+        batch_norm[calc_ncta1d(numSamples, 512), 512, custr](dA, daInorm, k)
 
         aInorm = daInorm.copy_to_host(stream=custr)
 
@@ -307,9 +260,8 @@ def spca(Vd, epsilon=0.1, d=3, k=10):
                 opt_x.fill(0)
 
                 # Only copy what we need
-                a = gpu_slice(dA, i).reshape(p, 1)
-                Ik = gpu_slice(dI, i).reshape(k, 1)
-                aIk = a[Ik]
+                Ik = dI[:, i].copy_to_host()
+                aIk = dA[:k, i].copy_to_host().reshape(k, 1)
                 opt_x[Ik] = (aIk / val)
 
         # Free allocations
@@ -324,7 +276,7 @@ def generate_input_file():
 
 
 def check_result():
-    dit = 3
+    dit = 1
     kit = 10
     A = np.load(cached_input_file)
     U, S, _ = np.linalg.svd(A)
@@ -344,14 +296,14 @@ def benchmark():
     U, S, _ = np.linalg.svd(A)
     Vd = U[:, 0:dit].dot(np.diag(np.sqrt(S[0:dit])))
 
-    print(min(timeit.repeat(lambda: spca(Vd, d=dit, k=kit), repeat=3,
+    print(min(timeit.repeat(lambda: spca(Vd, d=dit, k=kit), repeat=1,
                             number=1)))
     # Best CPU time 7.05 seconds
 
 
 def benchmarkLarge():
     A = np.load(cached_input_file)
-    dmax = 5
+    dmax = 3
     kmax = 50
     #SVD ONLY HERE
     p = A.shape[0]
@@ -388,37 +340,5 @@ def main():
         check_result()
 
 
-def test_sorter():
-    k = 3
-
-    n = 10
-    A = np.asfortranarray(np.random.rand(n, 1))
-    # A = np.array([[0.31255729],
-    #               [0.68038179],
-    #               [0.1824953],
-    #               [0.82793691],
-    #               [0.05213435],
-    #               [0.79801885],
-    #               [0.4090768],
-    #               [0.62787787],
-    #               [0.03544625],
-    #               [0.42592408]], dtype='float64')
-    I = np.zeros(k, dtype='int16', order='F').reshape(k, 1)
-    # I = np.zeros(n, dtype='int16', order='F').reshape(n, 1)
-    print(A)
-
-    expect = sorted(A.flatten().tolist())[-k:]
-    batch_k_selection[1, n](A, I, k)
-
-    print(I)
-    print(A[I])
-    got = A[I].flatten().tolist()
-
-    print(expect)
-    print(got)
-    assert set(expect) == set(got)
-
-
 if __name__ == '__main__':
     main()
-    # test_sorter()
