49a577f266b0a36f37ee56cfeef0293988acf00d
Siu Kwan Lam <michael.lam.sk@gmail.com>
2014-08-13 11:50:53 -0500

    Test simpler verson

--- spca.py
+++ spca.py
@@ -1,11 +1,16 @@
 from __future__ import print_function, division
 import sys
+import os
 import numpy as np
 import timeit
 import itertools
 import math
-from numbapro import cuda, float64, int16, int32
-from numbapro.cudalib import curand, cublas, sorting
+from numbapro import cuda, int32, float32, float64, void
+from numbapro.cudalib import curand, sorting
+
+cuda.select_device(int(os.environ.get("CUDA_DEVICE", 0)))
+NN = int(os.environ.get("NN", 1000))
+FILE = os.environ.get("FILE", "input{}.npy".format(NN))
 
 try:
     xrange
@@ -13,35 +18,39 @@ try:
 except NameError:
     xrange = range
 
-cached_input_file = "input.npy"
+cached_input_file = FILE  # "input.npy"
 
+float_type = float32
+float_dtype = np.float32
 
 def generate_input():
-    p = 5000
+    p = NN
     n = 100
-    X = np.random.randn(n, p)
+    X = np.random.randn(n, p).astype(float_dtype)
 
     A = (1. / n) * X.T.dot(X)
     U, S, tmp = np.linalg.svd(A)
-    A = U.dot(np.diag(S).dot(np.diag(1. / np.arange(1, p + 1)))).dot(
+    diag = np.diag(1. / np.arange(1, p + 1)).astype(float_dtype)
+    A = U.dot(np.diag(S).dot(diag)).dot(
         np.conjugate(U.T))
 
     print(A.shape)
+    print(A.dtype)
     return A
 
 
 def spca_unopt(Vd, epsilon=0.1, d=3, k=10):
     p = Vd.shape[0]
-    numSamples = (4. / epsilon) ** d
+    numSamples = int(math.ceil((4. / epsilon) ** d))
 
     ##actual algorithm
     opt_x = np.zeros((p, 1))
     opt_v = -np.inf
 
     #GENERATE ALL RANDOM SAMPLES BEFORE
-    C = np.random.randn(d, numSamples)
+    C = np.random.randn(d, numSamples).astype(float_dtype)
 
-    for i in np.arange(1, numSamples + 1):
+    for i in range(1, numSamples + 1):
 
         #c = np.random.randn(d,1)
         #c = C[:,i-1]
@@ -57,20 +66,65 @@ def spca_unopt(Vd, epsilon=0.1, d=3, k=10):
 
         if val > opt_v:
             opt_v = val
-            opt_x = np.zeros((p, 1))
+            opt_x = np.zeros((p, 1), dtype=float_dtype)
             opt_x[I[-k:]] = a[I[-k:], :] / val
 
     return opt_x
 
 
-@cuda.jit("void(float64[:,:], int32)")
+def spca_simpler(Vd, epsilon=0.1, d=3, k=10):
+    p = Vd.shape[0]
+    numSamples = int(math.ceil((4. / epsilon) ** d))
+
+    ##actual algorithm
+    opt_x = np.zeros((p, 1))
+    opt_v = -np.inf
+
+    # Prepare CUDA
+    prng = curand.PRNG()
+    custr = cuda.stream()
+    sorter = sorting.Radixsort(dtype=float_dtype, stream=custr)
+
+    #GENERATE ALL RANDOM SAMPLES BEFORE
+    # C = np.random.randn(d, numSamples).astype(float_dtype)
+    C = np.empty((d, numSamples), dtype=float_dtype)
+    prng.normal(C.ravel(), mean=0, sigma=1)
+
+    for i in range(1, numSamples + 1):
+
+        #c = np.random.randn(d,1)
+        #c = C[:,i-1]
+        c = C[:, i - 1:i]
+        c = c / np.linalg.norm(c)
+        a = Vd.dot(c)
+
+        #partial argsort in numpy?
+        #if partial, kth largest is p-k th smallest
+        #but need indices more than partial
+
+        # I = np.argsort(a, axis=0)
+        # val = np.linalg.norm(a[I[-k:]]) #index backwards to get k largest
+
+        I = sorter.argselect(a[:, 0], k=k, reverse=True)
+        custr.synchronize()
+
+        val = np.linalg.norm(a[:k]) #index to get k largest
+
+        if val > opt_v:
+            opt_v = val
+            opt_x = np.zeros((p, 1), dtype=float_dtype)
+            opt_x[I] = a[:k] / val
+
+    return opt_x
+
+@cuda.jit(void(float_type[:,:], int32))
 def norm_random_nums(C, d):
     i = cuda.grid(1)
     if i >= C.shape[1]:
         return
 
     c = C[:, i]
-    sum = 0.0
+    sum = float_type(0.0)
     for j in range(d):
         cj = c[j]
         sum += cj * cj
@@ -79,7 +133,7 @@ def norm_random_nums(C, d):
         c[j] /= val
 
 
-@cuda.jit("void(float64[:,:], float64[:,:], float64[:, :])")
+@cuda.jit(void(float_type[:,:], float_type[:,:], float_type[:, :]))
 def batch_matmul(Vd, C, A):
     sampleIdx = cuda.blockIdx.x
     tid = int32(cuda.threadIdx.x)
@@ -91,7 +145,7 @@ def batch_matmul(Vd, C, A):
     while tid < remain:
         j = tid + offset
 
-        sum = 0.0
+        sum = float_type(0.0)
         for k in range(C.shape[0]):
             sum += Vd[j, k] * C[k, sampleIdx]
 
@@ -101,28 +155,14 @@ def batch_matmul(Vd, C, A):
         offset += ntid
 
 
-@cuda.jit("void(float64[::1], int32, int32)", device=True)
-def swapf(ary, a, b):
-    t = ary[a]
-    ary[a] = ary[b]
-    ary[b] = t
-
-
-@cuda.jit("void(int16[::1], int32, int32)", device=True)
-def swapi(ary, a, b):
-    t = ary[a]
-    ary[a] = ary[b]
-    ary[b] = t
-
-
-@cuda.jit("void(float64[:,:], float64[:], int32)")
+@cuda.jit(void(float_type[:,:], float_type[:], int32))
 def batch_norm(A, aInorm, K):
     tid = cuda.grid(1)
 
     if tid >= A.shape[1]:
         return
 
-    sum = 0.0
+    sum = float_type(0.0)
     for k in range(K):
         val = A[k, tid]
         sum += val * val
@@ -134,14 +174,14 @@ def calc_ncta1d(size, blksz):
     return size + (blksz - 1) // blksz
 
 
-def spca(Vd, epsilon=0.1, d=3, k=10):
+def spca_full(Vd, epsilon=0.1, d=3, k=10):
     p = Vd.shape[0]
-    initNumSamples = int((4. / epsilon) ** d)
+    initNumSamples = int(math.ceil((4. / epsilon) ** d))
 
     maxSize = 32000
 
     ##actual algorithm
-    opt_x = np.zeros((p, 1))
+    opt_x = np.zeros((p, 1), dtype=float_dtype)
     opt_v = -np.inf
 
     # Send Vd to GPU
@@ -154,19 +194,25 @@ def spca(Vd, epsilon=0.1, d=3, k=10):
 
     rsstr1 = cuda.stream()
     rsstr2 = cuda.stream()
-    sorter1 = sorting.Radixsort(dtype=np.float64, stream=rsstr1)
-    sorter2 = sorting.Radixsort(dtype=np.float64, stream=rsstr2)
+    sorter1 = sorting.Radixsort(dtype=float_dtype, stream=rsstr1)
+    sorter2 = sorting.Radixsort(dtype=float_dtype, stream=rsstr2)
 
     while remaining:
         numSamples = min(remaining, maxSize)
         remaining -= numSamples
 
         # Prepare storage for vector A
-        dA = cuda.device_array(shape=(Vd.shape[0], numSamples), order='F')
-        dI = cuda.device_array(shape=(k, numSamples), dtype=np.int32,
+        # print(Vd.dtype)
+        # print('dA', (Vd.shape[0], numSamples))
+        # print('dI', (k, numSamples))
+
+        dA = cuda.device_array(shape=(Vd.shape[0], numSamples), order='F',
+                               dtype=Vd.dtype)
+        dI = cuda.device_array(shape=(k, numSamples), dtype=np.uint32,
                                order='F')
-        daInorm = cuda.device_array(shape=numSamples, dtype=np.float64)
-        dC = cuda.device_array(shape=(d, numSamples), order='F')
+        daInorm = cuda.device_array(shape=numSamples, dtype=Vd.dtype)
+        dC = cuda.device_array(shape=(d, numSamples), order='F',
+                               dtype=Vd.dtype)
 
         #GENERATE ALL RANDOM SAMPLES BEFORE
         # Also do normalization on the device
@@ -184,7 +230,6 @@ def spca(Vd, epsilon=0.1, d=3, k=10):
         # Replaces: I = np.argsort(a, axis=0)
         # Note: the k-selection is dominanting the time
 
-        lastprogress = 0
         custr.synchronize()
 
         # Distribute the work to two streams
@@ -193,12 +238,21 @@ def spca(Vd, epsilon=0.1, d=3, k=10):
         async_dI1 = dI.bind(rsstr1)
         async_dA2 = dA.bind(rsstr2)
         async_dI2 = dI.bind(rsstr2)
+        # selnext1 = sorter1.batch_argsort(dtype=dA.dtype,
+        #                                  count=dA.shape[0],
+        #                                  reverse=True)
+        # selnext2 = sorter2.batch_argsort(dtype=dA.dtype,
+        #                                  count=dA.shape[0],
+        #                                  reverse=True)
+
         selnext1 = sorter1.batch_argselect(dtype=dA.dtype,
-                                           count=dA.shape[0],
-                                           k=k, reverse=True)
+                                            count=dA.shape[0],
+                                            k=k,
+                                            reverse=True)
         selnext2 = sorter2.batch_argselect(dtype=dA.dtype,
-                                           count=dA.shape[0],
-                                           k=k, reverse=True)
+                                            count=dA.shape[0],
+                                            k=k,
+                                            reverse=True)
 
         def take2(it):
             it = iter(it)
@@ -212,9 +266,11 @@ def spca(Vd, epsilon=0.1, d=3, k=10):
             yield
             # print('compute', i)
             dIi = selnext(subdA)
+            # dIi = dIi.bind(stream) # sort
             yield
             # print('write', i)
-            dI[:, i].copy_to_device(dIi, stream=stream)
+            dI[:, i].copy_to_device(dIi, stream=stream) # select
+            # dI[:, i].copy_to_device(dIi[:k], stream=stream) # sort
             yield
 
         def exhaust(*args):
@@ -253,6 +309,7 @@ def spca(Vd, epsilon=0.1, d=3, k=10):
 
         custr.synchronize()
 
+        # print(aInorm)
         for i in xrange(numSamples):
             val = aInorm[i]
             if val > opt_v:
@@ -269,6 +326,8 @@ def spca(Vd, epsilon=0.1, d=3, k=10):
 
     return opt_x
 
+
+spca = spca_simpler
 
 def generate_input_file():
     A = generate_input()
@@ -290,28 +349,33 @@ def check_result():
 
 
 def benchmark():
-    dit = 3
+    dit = 2
     kit = 10
     A = np.load(cached_input_file)
     U, S, _ = np.linalg.svd(A)
     Vd = U[:, 0:dit].dot(np.diag(np.sqrt(S[0:dit])))
 
-    print(min(timeit.repeat(lambda: spca(Vd, d=dit, k=kit), repeat=1,
-                            number=1)))
+    funcs = [spca_unopt, spca_full, spca_simpler]
+    for fn in funcs:
+        print(min(timeit.repeat(lambda: fn(Vd, d=dit, k=kit), repeat=1,
+                                number=1)))
+
     # Best CPU time 7.05 seconds
 
 
 def benchmarkLarge():
     A = np.load(cached_input_file)
     dmax = 3
+    dmin = 2
     kmax = 50
+    kmin = 50
     #SVD ONLY HERE
     p = A.shape[0]
     U, S, _ = np.linalg.svd(A)
 
-    for dit in range(2, dmax + 1):
+    for dit in range(dmin, dmax + 1):
         Vd = U[:, 0:dit].dot(np.diag(np.sqrt(S[0:dit])))
-        for kit in range(10, kmax + 1, 10):
+        for kit in range(kmin, kmax + 1, 10):
             #eventually another loop for iterations
             #print(min(timeit.repeat(lambda: spca(Vd, d=dit, k=kit), repeat=3, number=1)))
             #print(min(timeit.repeat(lambda: spca_unopt(Vd, d=dit, k=kit), repeat=3, number=1)))
