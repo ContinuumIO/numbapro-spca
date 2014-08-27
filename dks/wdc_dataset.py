1ef48c1e88c9e664558a2694ab6c5e22e74ff574
Siu Kwan Lam <michael.lam.sk@gmail.com>
2014-08-27 15:28:27 -0500

    Refactor

--- dks/wdc_dataset.py
+++ dks/wdc_dataset.py
@@ -1,12 +1,17 @@
+from __future__ import print_function, absolute_import, division
+import sys
+import os
 import networkx as nx
 
 
-def parse_index(findex):
-    data = {}
+def parse_index_streaming(findex):
     for line in findex:
         name, num = (s.strip() for s in line.split())
-        data[int(num)] = name
-    return data
+        yield int(num), name
+
+
+def parse_index(findex):
+    return dict(parse_index_streaming(findex))
 
 
 def parse_arc(farc):
@@ -17,17 +22,55 @@ def parse_arc(farc):
     return conn
 
 
-def create_sample_graph():
-    with open("index.dat") as findex:
+def create_graph(idxfile, arcfile):
+    print("parsing indices")
+    with open(idxfile) as findex:
         nodes = parse_index(findex)
-
-    with open("arc.dat") as farc:
-        conn = parse_arc(farc)
 
     G = nx.Graph()
     G.add_nodes_from(nodes.values())
 
+    print("parsing arcs")
+    with open(arcfile) as farc:
+        conn = parse_arc(farc)
+
     edges = ((nodes[a], nodes[b]) for a, b in conn)
     G.add_edges_from(edges)
 
+    print("done building graph")
     return G
+
+
+def create_sample_graph():
+    return create_graph(indexfile="index.dat", arcfile="arc.dat")
+
+
+def subsample_from_arc(arcfile, idxfile, outfile):
+    nodes = set()
+    with open(arcfile) as farc:
+        conn = parse_arc(farc)
+    for a, b in conn:
+        nodes.add(a)
+        nodes.add(b)
+
+    # Filer the full index file to only contain the nodes that have edges
+    with open(idxfile) as fidx:
+        with open(outfile, 'w') as fout:
+            for num, name in parse_index_streaming(fidx):
+                if num in nodes:
+                    print("{name}\t{num}".format(num=num, name=name),
+                          file=fout)
+
+
+if __name__ == '__main__':
+    arcfile, idxfile, outfile = sys.argv[1:]
+    print('arcfile', arcfile)
+    print('idxfile', idxfile)
+    print('outfile', outfile)
+    if os.path.isfile(outfile):
+        print('{outfile} already exist'.format(outfile=outfile))
+        sys.exit(1)
+    if input('ok? > ') == 'y':
+        subsample_from_arc(arcfile, idxfile, outfile)
+    else:
+        print('abort')
