3b6bd605abba4b3e6443d68a510946e71de2ddd4
Siu Kwan Lam <michael.lam.sk@gmail.com>
2014-10-27 11:02:46 -0500

    Improve file builder efficiency

--- powermethod/build_files.py
+++ powermethod/build_files.py
@@ -1,33 +1,75 @@
 """
-Build tall skinny matrix
-"""
+Build gzip files for each column
 
-index = 'data/pld-index'
+"""
+# import zipfile
+from collections import deque
+import io
+import gzip
+
+index = 'pld-index'
+# index = 'pld-index-1m.dat'
 filename = 'pld-arc'
-output = 'data/arc-%d.dat'
+# filename = 'pld-arc-1m.dat'
+outfmt = 'data/{0}'
 
 start = 0
-stop = 10000000
+stop = 42889799 + 1
+# stop = 100
 
-with open(index) as findex:
-    for indexline in findex:
-        domain, node = indexline.split()
-        node = int(node)
-        print('node =', node)
-        if node < start:
-            continue
-        if node > stop:
-            break
-        with open(filename) as fin:
-            ofile = output % node
-            print(ofile)
-            with open(ofile, 'w') as fout:
-                ct = 0
-                for line in fin:
+
+class rewind_iterator(object):
+    def __init__(self, iterator):
+        self._iter = iter(iterator)
+        self._buffer = deque()
+        self._cur = None
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+
+        if self._buffer:
+            out = self._buffer.popleft()
+        else:
+            out = next(self._iter)
+
+        self._cur = out
+        return out
+
+    def unread(self):
+        assert self._cur is not None
+        self._buffer.append(self._cur)
+        self._cur = None
+
+
+with open(filename) as fin:
+    line_iter = iter(rewind_iterator(fin))
+    with open(index) as findex:
+        for indexline in findex:
+            domain, node = indexline.split()
+            node = int(node)
+            print('node =', node)
+            if node < start:
+                continue
+            elif node > stop:
+                break
+
+            ofile = outfmt.format(node)
+            print('ofile', ofile)
+            ct = 0
+            with io.BytesIO() as fout:
+                for line in line_iter:
                     a, b = map(int, line.split())
                     if a == node:
+                        fout.write("{0}\n".format(b).encode('utf8'))
                         ct += 1
-                        print(b, file=fout)
                     elif a > node:
+                        line_iter.unread()
                         break
-                print('count', ct)
+                fout.flush()
+                if ct > 0:
+                    print('ct =', ct)
+                    with gzip.open(ofile, 'wb') as gzfile:
+                        gzfile.write(fout.getvalue())
+
